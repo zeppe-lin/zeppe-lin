1de6b70369548c6bc47296814668def4c0f3b262
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-07-01 02:32:05 +0300

    dwmblocks: adjust to iosevka, fix deps

--- desktop/dwmblocks/audiovol.sh
+++ desktop/dwmblocks/audiovol.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 [ -x /usr/bin/amixer ] || exit 1
 
 if amixer get Master | grep -q off; then
-	echo "off"
+	printf "%b" "\ufa80"
 else
-	amixer get Master | tail -1 | sed 's/.*\[\([0-9]*\)%\].*/\1/'
+	printf "%b %i" \
+		"\ufa7d" \
+		"$(amixer get Master | tail -1 | sed 's/.*\[\([0-9]*\)%\].*/\1/')"
 fi
 
 # End of file.
--- desktop/dwmblocks/config.h
+++ desktop/dwmblocks/config.h
@@ -1,11 +1,11 @@
 /* Modify this file to change what commands output to your statusbar, and recompile using the make command. */
 static const Block blocks[] = {
 	/* Icon */	/* Command */				/* Update Interval */	/* Update Signal */
-	{"",		"/usr/share/dwmblocks//batt.sh",			2,			0},
-	{"audio:",	"/usr/share/dwmblocks/audiovol.sh",			2,			0},
-	{"cpumem:",	"/usr/share/dwmblocks/cpumem.sh",			2,			0},
-	{"diskfree:",	"/usr/share/dwmblocks/diskfree.sh",			15,			0},
-	{"",		"date '+%b %d (%a) %H:%M'",				5,			0},
+	{" ",		"/usr/share/dwmblocks/batt.sh",				2,			0},
+	{"",		"/usr/share/dwmblocks/audiovol.sh",			2,			0},
+	{"\uf85a ",	"/usr/share/dwmblocks/cpumem.sh",			2,			0},
+	{"\uf7c9 ",	"/usr/share/dwmblocks/diskfree.sh",			15,			0},
+	{"\uf5ef ",	"date '+%b %d (%a) %H:%M'",				5,			0},
 };
 
 /* Sets delimeter between status commands. NULL character ('\0') means no delimeter. */
