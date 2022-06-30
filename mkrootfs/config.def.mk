45cb1affe42bb8de501ce50f085c5556a4fc441e
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-07-01 01:22:59 +0300

    mkrootfs: mkrootfs: fix config.mk's paths

--- mkrootfs/config.def.mk
+++ mkrootfs/config.def.mk
@@ -2,7 +2,7 @@ ROOTFS		= /tmp/rootfs-$(shell date +%F)-$(shell uname -m)
 TARBALL		= $(ROOTFS).tar.xz
 LOGFILE		= $(ROOTFS).log
 PACKAGES	= $(shell pkgman --no-std-config \
-		  --config-set="pkgsrcdir /usr/src/pkgsrc/core" printf "%n\n")
+		  --config-set="pkgsrcdir /usr/src/zeppe-lin/core" printf "%n\n")
 
 PKGMKCONF	= /etc/pkgmk.conf
 PKGMANCONF	= /etc/pkgman.conf
