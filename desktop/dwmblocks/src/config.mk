2524b4537e9d6ccba3c256600abc1bb284a634bb
Alexandr Savca (chinarulezzz) <alexandr.savca89@gmail.com>
2022-07-09 22:43:00 +0300

    dwmblocks: add Makefile and config.mk, version bump

---
+++ desktop/dwmblocks/src/config.mk
@@ -0,0 +1,23 @@
+# dwmblocks version
+VERSION ?= git
+
+# customize below to fit your system
+
+# paths
+PREFIX = /usr/local
+DATAPREFIX = $(PREFIX)/share
+MANPREFIX = $(PREFIX)/share/man
+
+X11INC = /usr/X11R6/include
+X11LIB = /usr/X11R6/lib
+
+# flags
+CPPFLAGS = -I$(X11INC) -D_DEFAULT_SOURCE
+CFLAGS = -std=c99 -pedantic -Wall -Wextra -Wformat
+LDFLAGS = -L$(X11LIB)
+LDLIBS = -lX11
+
+# compiler and linker
+CC = cc
+
+# End of file.
